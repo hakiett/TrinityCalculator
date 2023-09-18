f1faaed7a362ab9b007982bfcdf3b6b3abf5d177
Kiet Ha <cupritix@gmail.com>
2023-09-18 15:32:09 -0400

    not done stack class have to relook at old notes

---
+++ src/main/java/edu/trinity/MyStack.java
@@ -0,0 +1,19 @@
+package edu.trinity;
+
+public class MyStack<T> {
+    public void push(T hello) {
+        return;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
